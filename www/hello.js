94ea1bd1d7059a2d031e40a11f511d6cc1e7a586
esterproceed <EsterD@proceed.co.il>
2026-02-02 23:02:13 +0200

    Update hello.js

--- www/hello.js
+++ www/hello.js
@@ -1,7 +1,11 @@
 /*global cordova, module*/
 
 module.exports = {
+
+    alert("before...");
     greet: function (name, successCallback, errorCallback) {
+            alert("greet...");
+
         cordova.exec(successCallback, errorCallback, "Hello", "greet", [name]);
     }
 };
